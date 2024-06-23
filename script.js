82d0e36c3ee989252ecea72e14bf341d3210e10e
Golla-Rakesh21 <164481514+Golla-Rakesh21@users.noreply.github.com>
2024-06-23 17:07:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let timeRef = document.querySelector(".timer-display");
-let int = null;
+var ms = 0, s = 0, m = 0, h = 0
+var timer;
 
-document.getElementById("start-timer").addEventListener("click", () => {
-    if(int !== null) {
-        clearInterval(int);
+var display = document.querySelector(".timer-Display")
+var laps = document.querySelector(".laps")
+
+
+function start(){
+    if(!timer){
+        timer = setInterval(run, 10)
     }
-    int = setInterval(displayTimer, 10);
-});
+}
 
-document.getElementById("pause-timer").addEventListener("click", () => {
-    clearInterval(int);
-});
-
-document.getElementById("reset-timer").addEventListener("click", () => {
-    clearInterval(int);
-    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
-}); 
-
-function displayTimer() {
-    milliseconds += 10;
-    if(milliseconds == 1000) {
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60) {
-            seconds = 0;
-            minutes++;
-            if(minutes == 60) {
-                minutes = 0;
-                hours++;
-            }
-        }
+function run(){
+    display.innerHTML = getTimer()
+    ms++              
+    if(ms == 100){
+        ms = 0
+        s++
     }
+    if(s == 60){
+        s = 0
+        m++
+    }
+    if(m == 60){
+        m = 0
+        h++
+    }
+    
+    if(h == 13){
+        h = 1
+    } 
+}
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = 
-        milliseconds < 10
-        ? "00" + milliseconds
-        : milliseconds < 100
-        ? "0" + milliseconds
-        : milliseconds;
+function getTimer(){
+    return (h<10 ? "0" + h: h) + " : " + (m<10 ? "0" + m : m) + " : " + (s<10 ? "0" + s : s) + " : " + (ms<10 ? "0" + ms : ms); 
+}
 
-    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
 
+
+function pause(){
+    stopTimer()  
+}
+
+function stopTimer(){
+    clearInterval(timer)
+    timer = false 
+}
+
+
+
+
+function reset(){
+    stopTimer()
+    ms = 0
+    s = 0
+    m = 0
+    h = 0
+    display.innerHTML = getTimer()
+}
+
+
+
+
+function restart(){
+    if(timer){ 
+        reset()
+        start()
+    }
+    
+}
+
+
+// lap = taking screenshot of current time...
+function lap() {
+    if(timer) {   
+        var Li = document.createElement("li")   
+        Li.innerHTML = getTimer() 
+        laps.appendChild(Li) 
+    }
+}
+
+function resetLap(){
+    laps.innerHTML = ""
 }
